b7ab71a1bfd8e440dd2a2db9667903f19eb2a66d
Wilson Mun <wilsonmungit@gmail.com>
2018-12-24 18:31:37 +1100

    sidenav initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {HeroesComponent} from './heroes/heroes.component';
+import {SidenavComponent} from './sidenav/sidenav.component';
+
+import {MaterialModule} from './material-module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    SidenavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
+  exports: [AppComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
